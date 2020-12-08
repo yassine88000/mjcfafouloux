ac6d00a837e0ff04f940705fdedc797360ef704c
yassine <yassine.sali.88@gmail.com>
2020-12-08 18:22:50 +0100

    css

--- index.php
+++ index.php
@@ -7,22 +7,23 @@
     <link rel="stylesheet" href="css/style.css" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
 </head>
-<body style="background-color: purple" >
+<body style="background-color: #550155" >
 
     <div class="events"><h1>Evenements à venir </h1></div>
 
 <div class="cards1">
     <ul>
         <li class="booking-card"
-             src="image/space.jpeg" >
+            style="background-image: url(image/space.jpeg)" width="100" height="200">
             <div class="book-container">
                 <div class="content">
                     <button class="btn">Réserver</button>
                 </div>
             </div>
+            //toutes les infos titre place date desc
             <div class="informations-container">
                 <h2 class="title">La tete dans les étoiles</h2>
-                <p class="sub-title">Et moi un je suis sous-titre</p>
+                <p class="sub-title">sfsfsfdfqdfqlfnqsinqsidsidsdpiqsddvdidqsyidvsisiovspfqpfbqpifvf</p>
                 <p class="price">
                     15 €
                 </p>
@@ -32,10 +33,6 @@
                             <p>Parc des expositions à NANTES</p>
                         </div>
                         <div class="box date">
-                            <svg class="icon" style="width:24px;height:24px" viewBox="0 0 24 24">
-                                <path fill="currentColor"
-                                      d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z"/>
-                            </svg>
                             <p>Samedi 1er février 2020</p>
                         </div>
                     </div>
@@ -45,7 +42,7 @@
             </div>
         </li>
         <li class="booking-card"
-            style="background-image: url(https://images.unsplash.com/photo-1580137197581-df2bb346a786?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ);">
+            style="background-image: url(image/space.jpeg);">
             <div class="book-container">
                 <div class="content">
                     <button class="btn">Réserver</button>
@@ -60,10 +57,7 @@
                 <div class="more-information">
                     <div class="info-and-date-container">
                         <div class="box info">
-                            <svg class="icon" style="width:24px;height:24px" viewBox="0 0 24 24">
-                                <path fill="currentColor"
-                                      d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z"/>
-                            </svg>
+
                             <p>La tete dans les étoiles</p>
                    -     </div>
                         <div class="box date">
@@ -76,7 +70,7 @@
             </div>
         </li>
         <li class="booking-card"
-            style="background-image: url(https://images.unsplash.com/photo-1579489225078-27977a77bf72?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ);">
+            style="background-image: url(image/space.jpeg);">
             <div class="book-container">
                 <div class="content">
                     <button class="btn">Réserver</button>
@@ -116,7 +110,7 @@
     <div class="cards2">
         <ul>
             <li class="booking-card"
-                style="background-image: url(https://images.unsplash.com/photo-1578944032637-f09897c5233d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ)">
+                style="background-image: url(image/space.jpeg)">
                 <div class="book-container">
                     <div class="content">
                         <button class="btn">Réserver</button>
@@ -147,7 +141,7 @@
                 </div>
             </li>
             <li class="booking-card"
-                style="background-image: url(https://images.unsplash.com/photo-1580137197581-df2bb346a786?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ);">
+                style="background-image: url(image/space.jpeg);">
                 <div class="book-container">
                     <div class="content">
                         <button class="btn">Réserver</button>
@@ -178,7 +172,7 @@
                 </div>
             </li>
             <li class="booking-card"
-                style="background-image: url(https://images.unsplash.com/photo-1579489225078-27977a77bf72?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ);">
+                style="background-image: url(image/space.jpeg);">
                 <div class="book-container">
                     <div class="content">
                         <button class="btn">Réserver</button>
