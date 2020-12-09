7e7ebb8a7005275584863f92e84413f58d55102f
yassine <yassine.sali.88@gmail.com>
2020-12-09 21:30:01 +0100

    Ajout commentaire + css sur les cartes + footer + header en cours

---
+++ entries.php
@@ -0,0 +1,26 @@
+<?php
+//include("fichier/header.php")
+//include("fichier/footer.php)
+?>
+<html>
+<head>
+    <title>Inscription</title>
+    <meta charset="utf-8"/>
+    <!--Font Space mono-->
+    <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
+    <!--Liaison Bootstrap-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--Liaison CSS-->
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body style="background-color: #550155" >
+<div class="events"><h1> Inscription à La tête dans les étoiles </h1></div>
+<div class="container">
+
+
+
+<h1>fkzOZK</h1>
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,101 +1,118 @@
 <?php
+//include("fichier/header.php")
 ?>
 <html>
 <head>
-    <title>Fafouloux</title>
+    <title>Inscription</title>
     <meta charset="utf-8"/>
 
+    <!--Font Space mono-->
     <link href="https://fonts.googleapis.com/css2?family=Space+Mono&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--Liaison bootstrap-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!--CSS -->
     <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
-<body style="background-color: #550155" >
-
-    <div class="events"><h1>Evenements à venir </h1></div>
+<!-- Background couleur-->
+<body style="background-color: #550155">
+<!--Titre -->
+<div class="events"><h1>Evenements à venir </h1></div>
 <div class="container">
+    <!--Cartes des évenements-->
     <div class="card-deck">
+        <!--Carte 1 -->
         <div class="card">
-            <img src="image/space.jpeg" class="bd-placeholder-img card-img-top" >
+            <img src="image/space.jpeg" class="bd-placeholder-img card-img-top">
             <ul class="card-text dateEvent">
-                <li>ceci est une date</li>
+                <li>le 12 Decembre 2020 21h00</li>
                 <li>ceci est une heure</li>
-                <li class="places">Il reste vido places</li>
+                <li class="places">Il reste 42 places</li>
             </ul>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title">La tête dans les étoiles</h5>
+                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to
+                    extra content. This content is a little longer. This content is a little bit longer.</p>
+                <p class="card-validate">JE PARTICIPE</p>
             </div>
         </div>
+        <!--Carte 2 -->
         <div class="card">
-            <img src="image/space.jpeg" class="bd-placeholder-img card-img-top" >
+            <img src="image/space.jpeg" class="bd-placeholder-img card-img-top">
             <ul class="card-text dateEvent">
                 <li>ceci est une date</li>
                 <li>ceci est une heure</li>
                 <li class="places">Il reste vido places</li>
             </ul>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title">La tête dans les étoiles</h5>
+                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to
+                    extra content. This content is a little longer. This content is a little bit longer.</p>
+                <p class="card-validate" href="fichier/entries.php">JE PARTICIPE</p>
             </div>
         </div>
+        <!--Carte 3 -->
         <div class="card">
-            <img src="image/space.jpeg" class="bd-placeholder-img card-img-top" >
+            <img src="image/space.jpeg" class="bd-placeholder-img card-img-top">
             <ul class="card-text dateEvent">
                 <li>ceci est une date</li>
                 <li>ceci est une heure</li>
                 <li class="places">Il reste vido places</li>
             </ul>
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                <h5 class="card-title">La tête dans les étoiles</h5>
+                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to
+                    extra content. This content is a little longer. This content is a little bit longer.</p>
+                <p class="card-validate">JE PARTICIPE</p>
             </div>
         </div>
-    <br>
-
-    <div class="card-deck">
-        <div class="card">
-            <img src="image/space.jpeg" class="bd-placeholder-img card-img-top" >
-            <ul class="card-text dateEvent">
-                <li>ceci est une date</li>
-                <li>ceci est une heure</li>
-                <li class="places">Il reste vido places</li>
-            </ul>
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+        <div class="card-deck">
+            <!--Carte 4 -->
+            <div class="card">
+                <img src="image/space.jpeg" class="bd-placeholder-img card-img-top">
+                <ul class="card-text dateEvent">
+                    <li>ceci est une date</li>
+                    <li>ceci est une heure</li>
+                    <li class="places">Il reste vido places</li>
+                </ul>
+                <div class="card-body">
+                    <h5 class="card-title">La tête dans les étoiles</h5>
+                    <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in
+                        to extra content. This content is a little longer. This content is a little bit longer.</p>
+                    <p class="card-validate">JE PARTICIPE</p>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img src="image/space.jpeg" class="bd-placeholder-img card-img-top" >
-            <ul class="card-text dateEvent">
-                <li>ceci est une date</li>
-                <li>ceci est une heure</li>
-                <li class="places">Il reste vido places</li>
-            </ul>
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <!--Carte 5 -->
+            <div class="card">
+                <img src="image/space.jpeg" class="bd-placeholder-img card-img-top">
+                <ul class="card-text dateEvent">
+                    <li>ceci est une date</li>
+                    <li>ceci est une heure</li>
+                    <li class="places">Il reste vido places</li>
+                </ul>
+                <div class="card-body">
+                    <h5 class="card-title">La tête dans les étoiles</h5>
+                    <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in
+                        to extra content. This content is a little longer. This content is a little bit longer.</p>
+                    <p class="card-validate">JE PARTICIPE</p>
+                </div>
             </div>
-        </div>
-        <div class="card">
-            <img src="image/space.jpeg" class="bd-placeholder-img card-img-top" >
-            <ul class="card-text dateEvent">
-                <li>ceci est une date</li>
-                <li>ceci est une heure</li>
-                <li class="places">Il reste vido places</li>
-            </ul>
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in to extra content. This content is a little longer. This content is a little bit longer.</p>
-                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <!--Carte 6 -->
+            <div class="card">
+                <img src="image/space.jpeg" class="bd-placeholder-img card-img-top">
+                <ul class="card-text dateEvent">
+                    <li>ceci est une date</li>
+                    <li>ceci est une heure</li>
+                    <li class="places">Il reste vido places</li>
+                </ul>
+                <div class="card-body">
+                    <h5 class="card-title">La tête dans les étoiles</h5>
+                    <p class="card-text">This is a longer card It's a broader card with text below as a natural lead-in
+                        to extra content. This content is a little longer. This content is a little bit longer.</p>
+                    <p class="card-validate">JE PARTICIPE</p>
+                </div>
             </div>
         </div>
     </div>
-</div>
 </body>
 </html>
